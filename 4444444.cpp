aedd7a6402113e7608b7b8d1ff9a277ad31d1a83
emmmoboi <133667674+emmmoboi@users.noreply.github.com>
2024-05-14 21:15:22 +0300

    Update 4444444.cpp
    changed the text for date entry error

--- 4444444.cpp
+++ 4444444.cpp
@@ -32,7 +32,7 @@ public:
 
     Date(int day, int month, int year) { // конструктор для установки даты открытия счета
         if (day < 1 || day > 31 || month < 1 || month > 12 || year < 0) { // проверка даты
-            throw invalid_argument("не существует такой даты ты чо ");
+            throw invalid_argument("Такой даты не существует");
         }
         this->day = day;
         this->month = month;
